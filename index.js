1f95c32ba66612538a775079710ef9d9345a5fc9
Memagog <55204188+Memagog@users.noreply.github.com>
2021-08-07 17:52:53 +0300

    created a structure to start the application server and connected the database

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const {mongoUrl} = require("./config/db_data")
+app.use(bodyParser.urlencoded({
+    extended: true
+}))  
+app.use(express.json());
+app.use(cors());
+
+const server =  async () => {
+    try {
+        await mongoose.connect(mongoUrl,{useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log(`Database is connected`))
+        await app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+    } catch (error) {
+        console.log(error)
+    }      
+}
+server();
+
+
