b2272b08310d12df2f8baf43d592212dee91009b
Memagog <55204188+Memagog@users.noreply.github.com>
2021-08-08 16:05:56 +0300

    create login function and compare passwords

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -9,20 +9,27 @@ const generateJsonWebToken = (payload) => {
     return jwt.sign(payload, secretKey, {expiresIn: "24h"});
 }
 const AuthController = {
+
     async registration(req,res) {
         try {
             const {username, email, password, roles} = req.body;
-            
+            const errors = validationResult(req);
+
+            if(!errors.isEmpty()){
+                return res.status(400).json({message: `Data isn't correct`})
+            }
+
             await User.findOne({email: email}).then((user)=>{
                 if(user){
                     return res.status(400).json({message: "This email already exists"})
                 }
                 else {
+                    const hashPassword = bcrypt.hashSync(password,7);
                     const newUser = new User({
                         _id: new mongoose.Types.ObjectId,
                         username: username,
                         email: email,
-                        password: password,
+                        password: hashPassword,
                         roles: roles,
                     });
                     newUser.save();
@@ -45,7 +52,35 @@ const AuthController = {
     },
 
     async login(req,res) {
-        await res.json({message: "Hello login"})
+        try {
+            const {email, password} = req.body;
+            await User.findOne({email: email}).then((user)=>{
+                // console.log(user)
+                if(!user){
+                    return res.status(400).json({message: `This account does not exist in the system, please register an account!`})
+                }
+                bcrypt.compare(password, user.password).then((isValid)=>{
+                    
+                    if(!isValid){
+                        return res.status(400).json({message: "Invalid password"})
+                    }
+                    else {
+                        const payload = {
+                            _id: user._id,
+                            username: user.username,
+                            email: user.email,
+                            password: user.password,
+                            roles: user.roles,
+                        }
+                        const token = generateJsonWebToken(payload);
+                        return res.json({token})
+                    }
+                }) 
+            })   
+        } catch (error) {
+            console.log(error);
+            return res.json({message: `Error: ${error}`})
+        }
     },    
 }
 
