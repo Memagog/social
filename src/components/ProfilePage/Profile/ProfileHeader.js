b4bf7466c6225f66cb55452a6eba442e564b4d06
Memagog <55204188+Memagog@users.noreply.github.com>
2021-08-15 22:38:49 +0300

    Create profile page

--- src/components/ProfilePage/Profile/ProfileHeader.js
+++ src/components/ProfilePage/Profile/ProfileHeader.js
@@ -11,7 +11,7 @@ const user = {
 export default function Profile() {
     return (
         <div className="profile">
-           <div className="profile__container">
+           <div className="profile__container">               
                 <div className="profile__container_img-block">
                     <img 
                     src="https://sun9-64.userapi.com/impg/mchhw5fLk4tZk2rAvYyR8XcpXHbLh8ejmBt3XA/3t8No7E3IbQ.jpg?size=888x1590&quality=96&sign=4b18e4fbccd484bc10974febcadbdd73&type=album" 
@@ -19,14 +19,16 @@ export default function Profile() {
                     className="profile__container_img-block_profile-img"
                     />
                 </div>
-                <div className="profile__container_info-block">
-                    <div className="profile__container_info-block_edit-button">
-                        <button>Edit</button>
-                    </div>
-                    <p>id: {user._id}</p>
-                    <p>Username: {user.name}</p>
-                    <p>Data: {user.data}</p>
-                    <p>followers: {user.followers} following: {user.following}</p>
+                <div className="profile__container_info-block">      
+                   <div className="profile__container_info-block_info-container">
+                        <p>id: {user._id}</p>
+                        <p>Username: {user.name}</p>
+                        <p>Data: {user.data}</p>
+                        <p>followers: {user.followers} following: {user.following}</p>
+                   </div>
+                   <div>
+                        <button className="profile__container_info-block_edit-button">Edit profile</button>
+                   </div>                     
                 </div>
            </div>
         </div>
